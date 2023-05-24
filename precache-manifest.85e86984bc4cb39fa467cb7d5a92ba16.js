99229c35826b0aec02169a3588191e0e0b1b725b
Code Sandbox <hello@codesandbox.io>
2023-05-24 14:11:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.85e86984bc4cb39fa467cb7d5a92ba16.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3017a2b9f0fbc50acd5c9713ba83ca14",
+    "url": "https://caiolauro.github.io/csb-8rhh4c/index.html"
+  },
+  {
+    "revision": "9a0226b9c81768c1a998",
+    "url": "https://caiolauro.github.io/csb-8rhh4c/static/js/2.8b594405.chunk.js"
+  },
+  {
+    "revision": "858bb05b65110cf2b388",
+    "url": "https://caiolauro.github.io/csb-8rhh4c/static/js/main.fc5abedf.chunk.js"
+  },
+  {
+    "revision": "fad258669061d948c6f2",
+    "url": "https://caiolauro.github.io/csb-8rhh4c/static/js/runtime-main.3c05e823.js"
+  }
+]);
